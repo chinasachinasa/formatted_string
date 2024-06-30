9478123c7fbd915fd03249f9fd71a9a5b6c42f7f
chinasachinasa <chinasachinasa2014@gmail.com>
2024-06-30 14:43:04 +0100

    learning list

---
+++ formatted.py
@@ -0,0 +1,18 @@
+# LISTS
+
+newlist = ['banana', 'mango','apple','orange']
+print(newlist)
+
+#ORDERED LIST
+
+list = ['watermelon','banana', 'mango','apple','orange','kiwi']
+print(list)
+
+#LIST LENGTH
+
+listname = ['cat','dog','lion','chicken','tiger']
+print(len(listname))
+
+employee_department = [4,5.8,9,True,5,'monkey']
+print(len(employee_department))
+
